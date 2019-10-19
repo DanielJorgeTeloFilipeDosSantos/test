9eca7ea4feba89599966ecfe7c215484bfcc34ff
Daniel Santos <danieljorgefilipesantos@gmail.com>
2019-10-19 14:34:21 +0100

    cleaning the code, so that it looks readable, deleting comments and optamizing the working code

--- src/components/VehicleList.js
+++ src/components/VehicleList.js
@@ -18,32 +18,31 @@ export default class VehicleList extends Component {
       });
     });
   }
-  
-  //   async componentDidMount() {
-//     getData(data => {
-// 		const data2 = JSON.parse(data)
-// 		const Array = []
-// 		data2.vehicles.map((res=>{
-// 			getDataById(res.id,data=>{
-// 				const obj1 = 
-// 					JSON.parse(data)
-// 				  const obj2 = {
-// 					res
-// 				  }
-// 				  const obj3=Object.assign({},obj1, obj2);
-// 				Array.push(obj3)
-// 			})
-// 		})
-//       this.setState({
-// 		data: Array
-// 	})
-// 	})	
-//   }
 
+  //   async componentDidMount() {
+  //     getData(data => {
+  // 		const data2 = JSON.parse(data)
+  // 		const Array = []
+  // 		data2.vehicles.map((res=>{
+  // 			getDataById(res.id,data=>{
+  // 				const obj1 =
+  // 					JSON.parse(data)
+  // 				  const obj2 = {
+  // 					res
+  // 				  }
+  // 				  const obj3=Object.assign({},obj1, obj2);
+  // 				Array.push(obj3)
+  // 			})
+  // 		})
+  //       this.setState({
+  // 		data: Array
+  // 	})
+  // 	})
+  //   }
 
   mapCarComponent() {
     return this.state.data.vehicles.map(carData => (
-		<div className="column" style={{backgroundColor:'white'}}>
+      <div className="column" style={{ backgroundColor: "white" }}>
         <Card data={carData} />
       </div>
     ));
@@ -55,19 +54,7 @@ export default class VehicleList extends Component {
       console.log(this.state.data);
       return (
         <div>
-			<div className="row">
-				{this.mapCarComponent()}
-			</div>
-
-
-
-
-
-
-
-
-
-
+          <div className="row">{this.mapCarComponent()}</div>
         </div>
       );
     }
